8b8a1e9021a01d79ddf4fdd67eb33775dd672fdb
Dimitris Karakaxas <karakaxasdimitrios@yahoo.com>
2022-05-10 12:21:35 +0300

    add: box shadow

--- src/components/InputSearch/InputSearch.jsx
+++ src/components/InputSearch/InputSearch.jsx
@@ -7,6 +7,7 @@ const InputSearch = () => {
       <Input
         placeholder="Search for a country..."
         classNames={{
+          wrapper: "shadow-lg",
           input:
             "bg-white dark:bg-dark-blue-dm dark:text-white py-6 pl-14 border-transparent",
           icon: "text-xl ml-4 dark:text-white",
